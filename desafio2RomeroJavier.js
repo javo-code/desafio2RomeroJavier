55a90f3f06fc6d1c6bb88afbf03de555fd5314ee
javo <romerofj26@gmail.com>
2023-10-21 15:12:17 -0300

    update

--- desafio2RomeroJavier.js
+++ desafio2RomeroJavier.js
@@ -19,21 +19,29 @@ class ProductManager {
     }
   }
 
-  async addProduct(product) {
-    try {
-      if (!title || !description || !price || !img || !code || !stock) {
-      console.log('Error: Todos los campos son obligatorios.');
+async addProduct(product) {
+  try {
+    const products = await this.getProducts();
+    const isCodeRepeated = products.some(existingProduct => existingProduct.code === product.code);
+    if (isCodeRepeated) {
+      console.log('Código de producto repetido!');
+      return; // Detener el proceso si el código está repetido
+    }
+    if (!product.title || !product.description || !product.price || !product.img || !product.code || !product.stock) {
+      const errorMessage = 'Error: Todos los campos son obligatorios.';
+      await fs.promises.writeFile(this.path, JSON.stringify([])); // Escribir un array vacío para evitar cambios en el archivo si hay un error.
+      console.log('SEGUNDA CONSULTA - AGREGO UN PRODUCTO AL ARCHIVO', await this.getProducts());
+      console.log(errorMessage);
       return; // Salir del método si algún campo está faltando
     }
-      const products = await this.getProducts();
-      product.id = this.nextId; // Asigno el ID autoincrementable al producto.
-      this.nextId++; // Incremento el valor del próximo ID.
-      products.push(product);
-      await fs.promises.writeFile(this.path, JSON.stringify(products));
-    } catch (error) {
-      console.log(error);
-    }
+    product.id = this.nextId; // Asigno el ID autoincrementable al producto.
+    this.nextId++; // Incremento el valor del próximo ID.
+    products.push(product);
+    await fs.promises.writeFile(this.path, JSON.stringify(products));
+  } catch (error) {
+    console.log(error);
   }
+}
 
   // Obtiene un producto por su ID.
 async getProductById(idProduct) {
@@ -85,7 +93,7 @@ const product1 = {
 title: "JERINGA",
 description: "Este es un producto prueba",
 price: 200,
-thumbnail: "Sin imagen",
+img: "https://www.google.com.ar/search?q=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&tbm=isch&ved=2ahUKEwiElsfo1oeCAxWsh5UCHajBBrsQ2-cCegQIABAA&oq=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&gs_lcp=CgNpbWcQAzoECCMQJ1CnCVi7cWDBeWgAcAB4AIABmAGIAfImkgEEMC4zNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=EAw0ZYTOCqyP1sQPqIOb2As&authuser=0&bih=738&biw=1536",
 code: "ACC0101",
 stock: 25
 };
@@ -94,7 +102,7 @@ const product2 = {
 title: "GASA",
 description: "Este es un producto prueba",
 price: 400,
-thumbnail: "Sin imagen",
+img: "https://www.google.com.ar/search?q=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&tbm=isch&ved=2ahUKEwiElsfo1oeCAxWsh5UCHajBBrsQ2-cCegQIABAA&oq=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&gs_lcp=CgNpbWcQAzoECCMQJ1CnCVi7cWDBeWgAcAB4AIABmAGIAfImkgEEMC4zNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=EAw0ZYTOCqyP1sQPqIOb2As&authuser=0&bih=738&biw=1536",
 code: "ACC0201",
 stock: 25
 };
@@ -103,7 +111,7 @@ const product3 = {
 title: "VENDA",
 description: "Este es un producto prueba",
 price: 2500,
-thumbnail: "Sin imagen",
+img: "https://www.google.com.ar/search?q=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&tbm=isch&ved=2ahUKEwiElsfo1oeCAxWsh5UCHajBBrsQ2-cCegQIABAA&oq=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&gs_lcp=CgNpbWcQAzoECCMQJ1CnCVi7cWDBeWgAcAB4AIABmAGIAfImkgEEMC4zNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=EAw0ZYTOCqyP1sQPqIOb2As&authuser=0&bih=738&biw=1536",
 code: "ACC0301",
 stock: 25
 };
@@ -112,7 +120,7 @@ const product4 = {
 title: "ALCOHOL EN GEL",
 description: "Este es un producto prueba",
 price: 100,
-thumbnail: "Sin imagen",
+img: "https://www.google.com.ar/search?q=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&tbm=isch&ved=2ahUKEwiElsfo1oeCAxWsh5UCHajBBrsQ2-cCegQIABAA&oq=PRODUCTOS+DESCARTABLES+ENFERMERIA+GASA+VENDA+JERINGA+ALCOHOL+EN+GEL&gs_lcp=CgNpbWcQAzoECCMQJ1CnCVi7cWDBeWgAcAB4AIABmAGIAfImkgEEMC4zNZgBAKABAaoBC2d3cy13aXotaW1nwAEB&sclient=img&ei=EAw0ZYTOCqyP1sQPqIOb2As&authuser=0&bih=738&biw=1536",
 code: "ACC0401",
 stock: 25
 };
@@ -120,28 +128,29 @@ stock: 25
 const test = async () => {
   console.log('PRIMER CONSULTA - MOSTRAR ARRAY VACIO', await productManager.getProducts());
   // addProduct
+  console.log('SEGUNDA CONSULTA - AGREGO PRODUCTOS AL ARCHIVO');
   await productManager.addProduct(product1);
-  console.log('SEGUNDA CONSULTA - AGREGO UN PRODUCTO AL ARCHIVO', await productManager.getProducts());
   await productManager.addProduct(product2);
-  console.log('TERCER CONSULTA - AGREGO DOS MAS', await productManager.getProducts());
   await productManager.addProduct(product3);
   await productManager.addProduct(product4);
-  
+  console.log(await productManager.getProducts());
+    
   // getProductById
   const foundProduct = await productManager.getProductById(3);
-  console.log('CUARTA CONSULTA - ENCONTRAR POR ID:', foundProduct);
+  console.log('TERCERA CONSULTA - ENCONTRAR POR ID:', foundProduct);
 
   // updateProduct - un solo campo.
-await productManager.updateProduct(4, {
+await productManager.updateProduct(2, {
   title: "PRODUCTO MODIFICADO",
   code: "NUEVO CODIGO DE ARTICULO",
-  price: 1000000
+  price: 1000000,
+  img: "No image"
 });
-  console.log('QUINTA CONSULTA - MODIFICAR PRODUCTO OBTENIDO POR ID Y MOSTRAR ARRAY ACTUALIZADO: ', await productManager.getProducts());
+  console.log('CUARTA CONSULTA - MODIFICAR PRODUCTO OBTENIDO POR ID Y MOSTRAR ARRAY ACTUALIZADO: ', await productManager.getProducts());
 
   // deleteProduct
-await productManager.deleteProduct(3);
-  console.log('SEXTA CONSULTA - ELIMINAR PRODUCTO POR ID Y MOSTRAR ARRAY ACTUALIZADO:', await productManager.getProducts());
+await productManager.deleteProduct(1);
+  console.log('QUINTA CONSULTA - ELIMINAR PRODUCTO POR ID Y MOSTRAR ARRAY ACTUALIZADO:', await productManager.getProducts());
 } 
 
 test();
